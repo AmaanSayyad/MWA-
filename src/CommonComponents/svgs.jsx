bb4cc39a09566364b773bf1c1ba6f093e3e9a78b
Amaan Sayyad <amaansayyad@yahoo.com>
2025-08-23 01:02:51 +0530

    feat: add reusable common components library
    - Implement Navbar.jsx for navigation component
    - Add NavDropDown.jsx for dropdown navigation menus
    - Include CustomModal.jsx for modal dialogs
    - Add FourButtons.jsx for button group components
    - Include SVG components and utility functions
    - Establish component library for consistent UI patterns

---
+++ src/CommonComponents/svgs.jsx
@@ -0,0 +1,70 @@
+export const ArrowUp = (
+  <svg
+    width="12"
+    height="7"
+    viewBox="0 0 12 7"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M11 1L6 6L1 1"
+      stroke="#1F2937"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export const ArrowDown = (
+  <svg
+    width="12"
+    height="7"
+    viewBox="0 0 12 7"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M11 6L6 1L1 6"
+      stroke="#1F2937"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export const clipBoard = (
+  <svg
+    width="12"
+    height="16"
+    viewBox="0 0 12 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M11.25 3.5V4.25H12V14.75H11.25V15.5H0.75V14.75H0V1.25H0.75V0.5H8.25V1.25H9V2H9.75V2.75H10.5V3.5H11.25ZM9.75 13.25H10.5V6.5H6V2H2.25V2.75H1.5V13.25H2.25V14H9.75V13.25ZM9 5V4.25H8.25V3.5H7.5V5H9ZM8.25 9.5V8H3.75V9.5H8.25ZM8.25 11.75V10.25H3.75V11.75H8.25Z"
+      fill="black"
+    />
+  </svg>
+);
+
+export const closeIcon = (
+  <svg
+    width="46"
+    height="46"
+    viewBox="0 0 46 46"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M40.25 23C40.25 32.5269 32.5269 40.25 23 40.25C13.4731 40.25 5.75 32.5269 5.75 23C5.75 13.4731 13.4731 5.75 23 5.75C32.5269 5.75 40.25 13.4731 40.25 23ZM14.6262 31.3738C14.2357 30.9832 14.2357 30.3501 14.6262 29.9596L21.5858 23L14.6262 16.0404C14.2357 15.6499 14.2357 15.0168 14.6262 14.6262C15.0168 14.2357 15.6499 14.2357 16.0404 14.6262L23 21.5858L29.9596 14.6262C30.3501 14.2357 30.9832 14.2357 31.3738 14.6262C31.7643 15.0168 31.7643 15.6499 31.3738 16.0404L24.4142 23L31.3738 29.9596C31.7643 30.3501 31.7643 30.9832 31.3738 31.3738C30.9832 31.7643 30.3501 31.7643 29.9596 31.3738L23 24.4142L16.0404 31.3738C15.6499 31.7643 15.0168 31.7643 14.6262 31.3738Z"
+      fill="#C6C6C8"
+    />
+  </svg>
+);
+
