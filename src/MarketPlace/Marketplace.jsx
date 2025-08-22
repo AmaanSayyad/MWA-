447e197b449b4564a9c861ac31e1818be79a7023
Amaan Sayyad <amaansayyad@yahoo.com>
2025-08-23 01:03:05 +0530

    feat: implement Marketplace trading interface
    - Add HeroSection.jsx for marketplace introduction
    - Implement FeatureAuthor.jsx for author showcase
    - Include SVG components for visual elements
    - Add constraint configurations for business logic
    - Create main Marketplace.jsx component with trading functionality

---
+++ src/MarketPlace/Marketplace.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import HeroSection from "./Components/HeroSection";
+import FeatureAuthor from "./Components/FeatureAuthor";
+
+
+const Marketplace = () => {
+
+
+  return (
+    <>
+    <div className="px-20 border-t border-x border-black rounded-t-[64px]">
+      <HeroSection />
+      <FeatureAuthor />
+    </div>
+    </>
+  );
+};
+
+export default Marketplace;
